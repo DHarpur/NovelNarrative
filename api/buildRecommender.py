fa7ca15e8fc792a4bd1e141c8f2772c420c1336b
Daniel O'Connor <danielharpuroconnor@gmail.com>
2020-03-23 01:20:38 +0000

    First draft of recommendation api, subject to change

---
+++ api/buildRecommender.py
@@ -0,0 +1,35 @@
+from surprise import SVD, Dataset, Reader, dump
+import surprise
+
+reader1 = Reader(line_format='user item rating', sep=',', skip_lines=1)
+bookRatingsData = Dataset.load_from_file('./data/book/ratings.csv', reader1)
+
+reader2 = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
+movieRatingsData = Dataset.load_from_file('./data/movie/ratings.csv', reader2)
+
+bookTrain, bookTest = surprise.model_selection.train_test_split(bookRatingsData, test_size=0.25)
+movieTrain, movieTest = surprise.model_selection.train_test_split(movieRatingsData, test_size=0.25)
+
+sim_options = {'name': 'pearson_baseline', 'user_based': False}
+algorithm1 = SVD()
+algorithm2 = SVD()
+
+kSplit = surprise.model_selection.split.KFold(n_splits=10, shuffle=True)
+
+print("Book Algorithm Training beginning...")
+algorithm1.fit(bookTrain)
+print("Book Algorithm Training complete!")
+print("Book Algorithm Testing beginning...")
+predictionsBook = algorithm1.test(bookTest)
+print("Book Algorithm Testing complete!")
+dump.dump("./RecommenderDump/algorithm1_dump", predictions=predictionsBook, algo=algorithm1)
+print("Book Algorithm and results sent to dump file")
+
+print("Movie Algorithm Training beginning...")
+algorithm2.fit(movieTrain)
+print("Movie Algorithm Training complete!")
+print("Movie Algorithm Testing beginning...")
+predictionsMovie = algorithm2.test(movieTest)
+print("Movie Algorithm Testing complete!")
+dump.dump("./RecommenderDump/algorithm2_dump", predictions=predictionsMovie, algo=algorithm2)
+print("Movie Algorithm and results sent to dump file")
---
+++ api/recommendation.py
@@ -0,0 +1,32 @@
+import surprise
+from collections import defaultdict
+import pandas as pd
+
+
+def get_top_n(predictions, n=10):
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+
+
+def get_book_title(book_id):
+    return bookData[book_id].title
+
+
+bookData = pd.read_csv("./data/book/books.csv", header=0)
+
+bookPredictions, bookAlgorithm = surprise.dump.load("./RecommenderDump/algorithm1_dump")
+
+
+topRecommendations = get_top_n(bookPredictions, n=10)
+
+for uid, user_ratings in topRecommendations.items():
+    print(uid, [get_book_title(int(iid)) for (iid, _) in user_ratings])
